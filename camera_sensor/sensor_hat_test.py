315a4eff20ee9f6090cf59676c81a197d854a279
YuheiHashimoto <188407340 +YuheiHashimoto@users.noreply.github.com>
2025-06-18 18:23:27 +0900

    sensor_hat_test

--- camera_sensor/sensor_hat_test.py
+++ camera_sensor/sensor_hat_test.py
@@ -1,3 +1,9 @@
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"  # ここでFB使用を回避！
+
 from sense_hat import SenseHat
+import time
+
 sense = SenseHat()
-sense.clear([0, 0, 255])  # 青色に全点灯
+sense.clear()
+sense.show_message("Hello!", text_colour=[255, 0, 0], back_colour=[0, 0, 0])
