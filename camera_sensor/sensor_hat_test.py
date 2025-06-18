d20270f6c40c231d25d5ac3c3be2fb03d356079c
YuheiHashimoto <188407340 +YuheiHashimoto@users.noreply.github.com>
2025-06-18 18:20:38 +0900

    sensor_hat_test

--- camera_sensor/sensor_hat_test.py
+++ camera_sensor/sensor_hat_test.py
@@ -1,7 +1,3 @@
 from sense_hat import SenseHat
-import time
-
 sense = SenseHat()
-
-# 背景色（黒）と文字色（赤）
-sense.show_message("Hello!", text_colour=[255, 0, 0], back_colour=[0, 0, 0])
+sense.clear([0, 0, 255])  # 青色に全点灯
