209176574821078f0264799244a79a9bd1c012d4
YuheiHashimoto <188407340 +YuheiHashimoto@users.noreply.github.com>
2025-06-18 18:41:32 +0900

    test

--- camera_sensor/sensor_hat_test.py
+++ camera_sensor/sensor_hat_test.py
@@ -1,16 +1,19 @@
-import os
-os.environ["SDL_VIDEODRIVER"] = "dummy"  # PygameにFBを使わせない（最重要）
-
 from sense_hat import SenseHat
-import time
-
 sense = SenseHat()
 
-# 全消灯しておく
-sense.clear()
+R = [255, 0, 0]  # 赤
+O = [0, 0, 0]    # 黒（消灯）
 
-# 青色で "Hello" を表示
-sense.show_message("Hello!", text_colour=[0, 0, 255], back_colour=[0, 0, 0])
+# "H" のような文字のパターン
+pixels = [
+    R, O, R, O, R, O, R, O,
+    R, O, R, O, R, O, R, O,
+    R, R, R, O, R, R, R, O,
+    R, O, R, O, R, O, R, O,
+    R, O, R, O, R, O, R, O,
+    R, O, R, O, R, O, R, O,
+    R, O, R, O, R, O, R, O,
+    R, O, R, O, R, O, R, O
+]
 
-# 消灯
-sense.clear()
+sense.set_pixels(pixels)
