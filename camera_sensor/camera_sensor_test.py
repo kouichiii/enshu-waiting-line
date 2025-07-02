4b82ec118f5eb69ebd4ec208b8df993e347dd885
YuheiHashimoto <188407340 +YuheiHashimoto@users.noreply.github.com>
2025-07-02 19:48:14 +0900

    camera_sensor

---
+++ camera_sensor/camera_sensor_test.py
@@ -0,0 +1,20 @@
+#センサのラズパイ送信処理 (テスト用)
+
+import requests
+
+url = "http://172.16.1.98:8000/api/upload" #URLは確認必須
+files = {
+    "image": open("sample1.jpg", "rb") #sample用の画像
+}
+data = {
+    "device_id": "1",
+    "temperature": "25.4",
+    "humidity": "65.0"
+}
+
+response = requests.post(url, files=files, data=data) #指定したURLにこのデータをHTTPでPOST
+
+print("Status Code:", response.status_code)
+print("Response Text:", response.text)  # ← JSONでない場合の内容がここに出る
+
+print(response.json())
